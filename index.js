4f31db7846b4e66a866732ede15f37846682db73
Shane Black <shane.black@hey.com>
2021-07-18 21:09:55 +1000

    3 passing, 2 failing - to be completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName()    {
+    return customerName.toUpperCase();
+}
+
+console.log(upperCaseCustomerName())
+
+function setBestCustomer()  {   
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer()    {
+    return bestCustomer = 'maybe bob';
+}
+
+console.log(bestCustomer)
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer()  {
+    const leastFavoriteCustomer= 'James';
+}
